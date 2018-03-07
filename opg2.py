8e9600dbb7af99a72e5e6cb7a4030a0915c60a0d
hallvsa <hallvsa@stud.ntnu.no>
2018-03-07 15:38:41 +0100

    Oppgave 2og3, 3 uferdig

--- opg2.py
+++ opg2.py
@@ -1,19 +1,43 @@
 import scipy as sp
+import numpy as np
 from scipy.sparse import spdiags
 from scipy.sparse import lil_matrix
 from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import spsolve
+
+
+
+density = 480
+L=2
+E = 1.3 *10**10
+w = 0.3
+d = 0.03
+g = 9.81
+I = (w*(d**3))/12
+f = -density*w*d*g
+
 
 
 def lagA(n):
     e = sp.ones(n)
     A = spdiags([e, -4 * e, 6 * e, -4 * e, e], [-2, -1, 0, 1, 2], n, n)
     A = lil_matrix(A)
-    B = csr_matrix([[1, 1, 1, 1],
-                    [1, 1, 1, 1],
-                    [1, 1, 1, 1]])
+    B = csr_matrix([[16, -9, 8/3, -1/4],
+                    [16/17, -60/17, 72/17, -28/17],
+                    [-12/17, 96/17, -156/16, 72/17]])
     A[0, 0:4] = B[0, :]
     A[n - 2, n - 4:n] = B[1, :]
     A[n - 1, n - 4:n] = B[2, :]
     return A
 
-print(lagA(10))
+
+def solveForYc(n, c):
+    A = csr_matrix(lagA(n))
+    h = L/n
+    const = (h**4)/(E*I) * f
+    b = np.array([const]*n).T
+    B = A.todense()
+    y = spsolve(A, b)
+    return y[c]
+
+print(solveForYc(10, 9))
