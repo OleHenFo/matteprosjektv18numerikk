91b7a195478d5b8a06b1d67f04c7993d9443799f
Potet <simen.m.storvik@gmail.com>
2018-03-14 12:50:01 +0100

    oppg4

--- Oppg4.py
+++ Oppg4.py
@@ -16,7 +16,7 @@ I = (w*(d**3))/12
 f = -density*w*d*g
 
 def korrektY(f,E,I,x,L):
-    return (f/(23*E*I))*x**2*(x**2-4*L*x+6*L**2)
+    return (f/(24*E*I))*x**2*(x**2-4*L*x+6*L**2)
 
 x = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
 y_e = len(x) * [0]
