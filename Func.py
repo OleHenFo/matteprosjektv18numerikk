a8ff70b9379dc63014e6259d46ae5ee382e09245
hallvsa <hallvsa@stud.ntnu.no>
2018-03-22 15:50:19 +0100

    Mindre fikser

--- Func.py
+++ Func.py
@@ -6,12 +6,11 @@ from scipy.sparse import lil_matrix
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
 
-
 n = 10
 
 density = 480
 L = 2
-h = L/n
+h = L / n
 E = 1.3 * (10 ** 10)
 w = 0.3
 d = 0.03
@@ -44,29 +43,33 @@ def solveForYc(n):
 
 def lagFasit(n):
     svar = np.zeros(n)
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         x = (L / n) * i
-        svar[i-1] = ((f * (x ** 2)) / (24 * E * I)) * ((6 * (L ** 2)) - (4 * L * x) + x ** 2)
+        svar[i - 1] = ((f * (x ** 2)) / (24 * E * I)) * ((6 * (L ** 2)) - (4 * L * x) + x ** 2)
     return svar
 
 
 def linsolv():
     nyN = 10
     list = []
-    while nyN < 10 * 2 ** 11:
+    fasitList = []
+    while nyN <= 10 * 2 ** 11:
         list.append(solveForYc(nyN)[nyN - 1])
+        fasitList.append(lagFasit(nyN)[nyN - 1])
         nyN *= 2
-    return list
+    return list, fasitList
 
 
 def findMaxError():
-    list = linsolv()
-    ans = lagFasit(10)[9]
+    list, fasitList = linsolv()
+    errorList = np.zeros(len(list))
     for i in range(0, len(list)):
-        list[i] -= ans
-    maxIndex = np.argmax(np.abs(list))
-    max = np.max(np.abs(list))
-    return max, maxIndex, list
+        errorList[i] = list[i]-fasitList[i]
+    maxIndex = np.argmax(np.abs(errorList))
+    minIndex = np.argmin(np.abs(errorList))
+    min = np.min(np.abs(errorList))
+    max = np.max(np.abs(errorList))
+    return max, maxIndex, errorList, minIndex, min
 
 
 def getConditionNumber():
@@ -74,23 +77,23 @@ def getConditionNumber():
     condlist = []
     while nyN < 5000:
         m = lagA(nyN).todense()
-        condNr = np.linalg.cond(m,1)
+        condNr = np.linalg.cond(m, 1)
         condlist.append(int(condNr))
         nyN *= 2
     return condlist
 
 
 def withADudeOnIt():
-    myN = 1280 # Using n = 20, to correctly add the force of the person, while keeping the condition number lowest possible
+    myN = 1280  # Using n = 20, to correctly add the force of the person, while keeping the condition number lowest possible
     A = csr_matrix(lagA(myN))
     myH = L / myN
     const = ((myH ** 4) / (E * I))
     b = np.array([f] * myN)
     edge = 1.7
     for i in range(myN):
-        index = i*myH
-        if(index>edge and index<L):
-            b[i] -= g*50/0.3
+        index = i * myH
+        if (index > edge and index < L):
+            b[i] -= g * 50 / 0.3
         b[i] *= const
     y = spsolve(A, b)
     return y
@@ -99,13 +102,13 @@ def withADudeOnIt():
 def withASineOnItNum(nn):
     p = 100
     A = csr_matrix(lagA(nn))
-    hh = L/nn
+    hh = L / nn
     const = ((hh ** 4) / (E * I))
-    b = np.array([f]*nn)
-    for i in range(1, nn+1):
+    b = np.array([f] * nn)
+    for i in range(1, nn + 1):
         x = (L / nn) * i
-        b[i-1] -= (p * g * np.sin((np.pi/L)*x))
-        b[i-1] = b[i-1] * const
+        b[i - 1] -= (p * g * np.sin((np.pi / L) * x))
+        b[i - 1] = b[i - 1] * const
     y = spsolve(A, b)
     return y
 
@@ -113,9 +116,12 @@ def withASineOnItNum(nn):
 def withASineOnItFasit(nn):
     p = 100
     y = np.zeros(nn)
-    for i in range(1, nn+1):
+    for i in range(1, nn + 1):
         x = (L / nn) * i
-        y[i-1] = ((f/(24*E*I))*x**2) * (x**2 - 4*L*x + 6*L**2) - ((p*g*L)/(E*I*np.pi))*(((L**3)/(np.pi**3))*np.sin((np.pi/L)*x) - (x**3)/6 + (L/2)*x**2 - ((L**2)/(np.pi**2))*x)
+        y[i - 1] = ((f / (24 * E * I)) * x ** 2) * (x ** 2 - 4 * L * x + 6 * L ** 2) - (
+                    (p * g * L) / (E * I * np.pi)) * (
+                               ((L ** 3) / (np.pi ** 3)) * np.sin((np.pi / L) * x) - (x ** 3) / 6 + (L / 2) * x ** 2 - (
+                                   (L ** 2) / (np.pi ** 2)) * x)
     return y
 
 
@@ -129,25 +135,27 @@ def errorInSine(nn):
 def allErrorsInSine():
     nyNN = 10
     errorList = []
-    while nyNN<22000:
-        errorList.append(errorInSine(nyNN)[nyNN-1])
-        nyNN *=2
+    while nyNN < 22000:
+        errorList.append(errorInSine(nyNN)[nyNN - 1])
+        nyNN *= 2
     return errorList
+
 
 def teoretiskFeil():
     minN = 10
-    errorList =[]
-    while minN<22000:
-        errorList.append((L**2)/minN**2)
-        minN *=2
+    errorList = []
+    while minN < 22000:
+        errorList.append((L ** 2) / minN ** 2)
+        minN *= 2
 
     print(errorList)
     return errorList
 
+
 def finnFinN():
     errors = allErrorsInSine()
     minIndex = np.argmin(np.abs(errors))
-    return (2**minIndex)*10
+    return (2 ** minIndex) * 10
 
 
 # Oppgave 2
@@ -169,7 +177,7 @@ print("")
 
 # Oppgave 4 (Må fikses)
 print("Oppgave 4: ")
-xx = np.arange(0,n,1)
+xx = np.arange(0, n, 1)
 yy = lagFasit(n)
 pl.plot(xx, yy)
 pl.plot(y_c)
@@ -184,13 +192,15 @@ for e in y_e:
 print()
 
 print("Nummerisk fjerdederivert (1/h^4)*Ay_e:")
+nummeriskFjerdederivert = 1 / h ** 4 * np.dot(A.todense(), y_e)
+print(nummeriskFjerdederivert)
 nummeriskFjerdederivert = 1/h**4 * np.dot(A.todense(), y_e)
 for i in range(0,9):
     print("{0:.16f}".format(nummeriskFjerdederivert.item(i)))
 print()
 
 print("Eksakt fjerdederivert f/EI:")
-eksaktFjerdederivert = [f/(E*I)]*10
+eksaktFjerdederivert = [f / (E * I)] * 10
 print(eksaktFjerdederivert)
 print()
 
@@ -203,8 +213,10 @@ print("Forward error:")
 forwardError = np.max(np.abs(feilVektor))
 print(forwardError)
 print("Relativ error:")
-relForErr = forwardError/(np.max(np.abs(eksaktFjerdederivert)))
+relForErr = forwardError / (np.max(np.abs(eksaktFjerdederivert)))
 print(relForErr)
+backwardsError = 2 ** -52
+print(relForErr / backwardsError)
 backwardsError = 2**-52
 print(relForErr/backwardsError)
 print()
@@ -218,22 +230,22 @@ print("")
 
 # Oppgave 5
 print("Oppgave 5: ")
-error, errorIndex, errorList = findMaxError()
+error, errorIndex, errorList, minIndex,min= findMaxError()
 condList = getConditionNumber()
 print("List of errors at x= L with n multiplying by two for each element:")
 print(errorList)
-print ("The largest error is "+str(error)+", which is number "+ str(errorIndex)+" in our list. This makes sense when we "
-                                                                          "look at the condition number for the different matrices : \n    ")
+print("The largest error is " + str(error) + ", which is number " + str(
+    errorIndex) + " in our list. The smallest error is at "+str(minIndex)+" with value "+str(min)+"This makes sense when we "
+                  "look at the condition number for the different matrices : \n    ")
 print(condList)
 print("Note that we stopped at n = 1280 as computations take too long for larger matrices ")
 print("--------------------------------")
 print("")
 
-
 # Oppgave 6
 print("Oppgave 6:")
 nn = 10
-xx = np.arange(0,nn,1)
+xx = np.arange(0, nn, 1)
 yy = withASineOnItFasit(nn)
 yy2 = withASineOnItNum(nn)
 pl.plot(xx, yy, 'b')
@@ -242,14 +254,14 @@ pl.axis([0, nn, -0.15, 0.01])
 pl.title("With a Sine on it")
 pl.show()
 print("Error")
-condAmach= np.zeros(len(condList))
-for i in range (len(condList)):
-    condAmach[i] = condList[i]*2**(-52)
+condAmach = np.zeros(len(condList))
+for i in range(len(condList)):
+    condAmach[i] = condList[i] * 2 ** (-52)
 yy = allErrorsInSine()
-xx = (2**np.arange(0,len(allErrorsInSine()),1))*10
-xx1 = (2**np.arange(0,len(allErrorsInSine())-3,1))*10
+xx = (2 ** np.arange(0, len(allErrorsInSine()), 1)) * 10
+xx1 = (2 ** np.arange(0, len(allErrorsInSine()) - 3, 1)) * 10
 
-print("CondAmach: "+str(condAmach))
+print("CondAmach: " + str(condAmach))
 print(yy)
 pl.loglog(xx, yy)
 pl.loglog(xx1, condAmach, 'g')
@@ -265,7 +277,7 @@ print("")
 print("Oppgave 7:")
 print("Position of the board with a man on top of it:")
 print(withADudeOnIt())
-xx = np.arange(0, 1280 ,1)
+xx = np.arange(0, 1280, 1)
 yy = withADudeOnIt()
 pl.plot(xx, yy)
 pl.axis([0, 1280, -0.2, 0.02])
@@ -273,4 +285,3 @@ pl.title("With a Dude on it")
 pl.show()
 print("--------------------------------")
 print("")
-
